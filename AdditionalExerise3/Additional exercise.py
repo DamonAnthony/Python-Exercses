80a82ef32abc547e6dae8921e4b50183956bd3ad
Damon Anthony <damonanthony125@gmail.com>
2020-03-09 16:12:56 +0200

    IntroExercises

---
+++ AdditionalExerise3/Additional exercise.py
@@ -0,0 +1,7 @@
+import math
+a = 8
+b = 2
+c = 1
+d = 4
+Ans = (math.sqrt(a - 3)/(b ^ 2 + c ^ 2 + d ^ 2))
+print(Ans)
---
+++ RadiusOfCircle/RadiusOfCircle.py
@@ -0,0 +1,5 @@
+import math
+
+r = float(input("Enter Radius "))
+a = math.pi*(r*r)
+print(a)
